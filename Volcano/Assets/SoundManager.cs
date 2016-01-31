640b528c0d63744464e5c08b26496cb4b7976a4a
gdeioannes <Trendingames@Trendingames.local>
2016-01-31 10:27:27 -0300

    Sound Manager Integration

---
+++ Volcano/Assets/SoundManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour {
+
+	private AudioSource[] audio;
+
+	public static SoundManager _instance;
+
+	// Use this for initialization
+	void Start () {
+		audio = this.gameObject.GetComponents<AudioSource>();
+		playMainTheme();
+		if(_instance==null){
+			_instance=this;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playMainTheme(){
+		audio[0].Play();
+	}
+
+	public void playVolcanoAngry(){
+		audio[1].Play();
+	}
+
+	public void playPeopleGrount(){
+		audio[2].Play();
+	}
+
+	public void playHappyPeople(){
+		audio[3].Play();
+	}
+
+	public SoundManager getSoundManager(){
+		return _instance;
+	}
+
+}
